940327029d4fee703a07f79412e0b15e3edf166a
poetmaddiep <poet.maddiep@gmail.com>
2024-12-16 15:39:00 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+// Gallery Navigation
+const images = ['images/ryan1.jpg', 'images/ryan2.jpg', 'images/ryan3.jpg'];
+let currentImageIndex = 0;
+
+document.getElementById('prev-btn').addEventListener('click', () => {
+    currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
+    document.getElementById('gallery-image').src = images[currentImageIndex];
+});
+
+document.getElementById('next-btn').addEventListener('click', () => {
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+    document.getElementById('gallery-image').src = images[currentImageIndex];
+});
+
+// Performances Section
+fetch('performances.json')
+    .then(response => response.json())
+    .then(data => {
+        const performanceList = document.getElementById('performance-list');
+        data.performances.forEach(performance => {
+            const listItem = document.createElement('li');
+            listItem.textContent = `${performance.date} - ${performance.venue}`;
+            performanceList.appendChild(listItem);
+        });
+    });
 
